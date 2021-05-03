4801ed0fa20ff19114bee04861cdc086d827ea7a
dalao <dalao1002@gmail.com>
2021-05-03 11:24:25 +0200

    save data when input is less than 4 bytes

--- src/spkr-fs.c
+++ src/spkr-fs.c
@@ -1,8 +1,22 @@
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 #include "spkr-fs.h"
 #include "spkr-fifo.h"
 
-struct mutex mutexForWriteSession;
+static ssize_t intspkr_read(struct file *file, char __user *buf, size_t size, loff_t *f_pos)
+{
+    printk(KERN_INFO "intspkr read!\n");
+    return 0;
+}
+
+typedef struct
+{
+    char soundData[4];
+    int copied;
+} PrivateData;
+
+static struct mutex mutexForWriteSession;
+
 static int intspkr_open(struct inode *inode, struct file *file)
 {
     if (file->f_mode & FMODE_WRITE)
@@ -12,31 +26,49 @@ static int intspkr_open(struct inode *inode, struct file *file)
             return -EBUSY;
         }
     }
+    file->private_data = kmalloc(sizeof(PrivateData), GFP_KERNEL);
+    ((PrivateData *)(file->private_data))->copied = 0;
     printk(KERN_INFO "intspkr opened!\n");
     return 0;
 }
 
-static ssize_t intspkr_read(struct file *filp, char __user *buf, size_t size, loff_t *f_pos)
+static inline void playSound(char *soundData)
 {
-    printk(KERN_INFO "intspkr read!\n");
-    return 0;
+    putSound((((uint16_t)soundData[0]) << 8) | ((uint16_t)soundData[1]), (((uint16_t)soundData[2]) << 8) | ((uint16_t)soundData[3]));
 }
-
-static ssize_t intspkr_write(struct file *filp, const char __user *buf, size_t size, loff_t *f_pos)
+static inline size_t copySoundDataFromUserSpace(PrivateData *privateData, const char __user *buf, const size_t remaining)
 {
-    char soundData[4];
-    ssize_t count;
-    ssize_t notCopied = 0;
-    printk(KERN_INFO "intspkr try to write %d bytes!\n", size);
-    for (count = 0; (size - count) >= 4; count += 4)
+    size_t notCopied;
+    size_t needToCopy = 4 - privateData->copied;
+    needToCopy = needToCopy > remaining ? remaining : needToCopy;
+    printk(KERN_INFO "Try to copy %d bytes from user space with %d offset", needToCopy, privateData->copied);
+    notCopied = copy_from_user(privateData->soundData + privateData->copied, buf, needToCopy);
+    printk(KERN_INFO "The result of copy_from_user is %d.\n", notCopied);
+    if (notCopied > 0)
     {
-        notCopied = copy_from_user(soundData, buf + count, 4);
-        if (notCopied > 0)
+        printk(KERN_INFO "intspkr failed to copy %d bytes from user space!\n", notCopied);
+        return needToCopy - notCopied;
+    }
+    return needToCopy;
+}
+static ssize_t intspkr_write(struct file *file, const char __user *buf, size_t size, loff_t *f_pos)
+{
+    ssize_t count = 0;
+    size_t copied;
+    PrivateData *privateData = (PrivateData *)(file->private_data);
+    printk(KERN_INFO "intspkr try to write %d bytes!\n", size);
+
+    while (size > count)
+    {
+        copied = copySoundDataFromUserSpace(privateData, buf + count, size - count);
+        count += copied;
+        printk(KERN_INFO "intspkr has written %d bytes out of %d bytes!\n", count, size);
+        privateData->copied += copied;
+        if (privateData->copied == 4)
         {
-            printk(KERN_INFO "intspkr failed to copy %d bytes from user space!\n", notCopied);
-            return count;
+            playSound(privateData->soundData);
+            privateData->copied -= 4;
         }
-        putSound((((uint16_t)soundData[0]) << 8) | ((uint16_t)soundData[1]), (((uint16_t)soundData[2]) << 8) | ((uint16_t)soundData[3]));
     }
     printk(KERN_INFO "intspkr wrote %d bytes!\n", count);
     return count;
@@ -48,7 +80,7 @@ static int intspkr_release(struct inode *inode, struct file *file)
     {
         mutex_unlock(&mutexForWriteSession);
     }
-    //kfree((*filp).private_data);
+    kfree(file->private_data);
     printk(KERN_INFO "intspkr released!\n");
     return 0;
 }
